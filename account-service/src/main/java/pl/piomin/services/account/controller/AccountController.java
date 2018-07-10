f12fa9a2f7344733c33997ac1012227610c57ce0
Artsiom Artsemyeu <artsiom.artsemyeu@netcracker.com>
2018-07-10 11:40:27 +0300

    featured system

--- account-service/src/main/java/pl/piomin/services/account/controller/AccountController.java
+++ account-service/src/main/java/pl/piomin/services/account/controller/AccountController.java
@@ -1,47 +1,45 @@
 package pl.piomin.services.account.controller;
 
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import pl.piomin.services.account.model.Account;
 import pl.piomin.services.account.repository.AccountRepository;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-@RestController
-public class AccountController {
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
+@RestController
+@RequestMapping("/accounts")
+public class AccountController {
 	private static final Logger LOGGER = LoggerFactory.getLogger(AccountController.class);
 	
 	@Autowired
 	private AccountRepository repository;
 
-	@GetMapping("/customer/{customer}")
+	@RequestMapping(value = "/customer/{customer}", method = GET)
 	public Flux<Account> findByCustomer(@PathVariable("customer") String customerId) {
 		LOGGER.info("findByCustomer: customerId={}", customerId);
 		return repository.findByCustomerId(customerId);
 	}
 
-	@GetMapping
+	@RequestMapping(method = GET)
 	public Flux<Account> findAll() {
 		LOGGER.info("findAll");
 		return repository.findAll();
 	}
 
-	@GetMapping("/{id}")
+	@RequestMapping(value = "/{id}", method = GET)
 	public Mono<Account> findById(@PathVariable("id") String id) {
 		LOGGER.info("findById: id={}", id);
 		return repository.findById(id);
 	}
 
-	@PostMapping
+	@RequestMapping(method = POST)
 	public Mono<Account> create(@RequestBody Account account) {
 		LOGGER.info("create: {}", account);
 		return repository.save(account);
